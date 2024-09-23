867a0a432f2dcb0b0de17b135e785ee830ced636
성민 류 <baekhun0330@naver.com>
2024-09-23 16:16:51 +0900

    류성민 : NumberCount 실습

--- Practice/000_StringCount/000_StringCount.cpp
+++ Practice/000_StringCount/000_StringCount.cpp
@@ -6,10 +6,36 @@ int StringCount(const char* const _Str)
 
     while (_Str[Count])
     {
+        if (0 == _Str[Count])
+        {
+            break;
+        }
+        Count++;
+    }
+    return Count;
+}
+
+int StringCount1(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
         if ('\0' == _Str[Count])
         {
             break;
         }
+        Count++;
+    }
+    return Count;
+}
+
+int StringCount0(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
         Count++;
     }
     return Count;
@@ -21,8 +47,16 @@ int main()
     // [B][B][B][B][0]
 
     int Result = StringCount("AAAA");
+    int Result0 = StringCount0("AAAA");
+
 
     //int Count = strlen("BBBB");
 
+
+    'A'; // 멀티 바이트
+    "A"; // 멀티바이트 - 문자열
+    L"A"; // 와이드 바이트 : 유니코드 != 인코딩
+    // 문자를 표현하는 표준
+    // 유니코드의 인코딩 방식은 UTF-8, UTF-16
     int a = 0;
 }
---
+++ Practice/001_NumberCount/001_NumberCount.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+// 음수 안함
+// 0 이외의 양수의 자릿수를 세자.
+int NumberCount(int _Value)
+{
+    // /(나누기를 최대한) 연산을 사용하세요.
+    int Result = 0;
+
+    while (true)
+    {
+        _Value /= 10;
+        Result += 1;
+
+        if (0 == _Value )
+        {
+            return Result;
+        }
+    }
+
+}
+
+// 해도 되고 안해도 됩니다.
+// 내 기준으로 지금
+// _Value 를 넣어주면 Buffer에 
+// 숫자를 문자열로 만드는 함수.
+void NumberToString(char* Buffer, int BufferSize, int _Value)
+{
+    return;
+}
+
+int main()
+{
+
+    // 5
+    int Result0 = NumberCount(12358);
+    // 4
+    int Result1 = NumberCount(5258);
+    // 7
+    int Result2 = NumberCount(5258111);
+
+    // 0은 영
+
+    char Buffer[100] = { 0 };
+
+    NumberToString(Buffer, 100, 3712);
+    // Buffer == "3712"
+}
