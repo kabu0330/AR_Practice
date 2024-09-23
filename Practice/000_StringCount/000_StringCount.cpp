97146d9296750ec2c0023bfce0dc98bcd5595cda
성민 류 <baekhun0330@naver.com>
2024-09-23 14:31:23 +0900

    000_StringCount

---
+++ Practice/000_StringCount/000_StringCount.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int StringCount(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
+        if ('\0' == _Str[Count])
+        {
+            break;
+        }
+        Count++;
+    }
+    return Count;
+}
+
+
+int main()
+{
+    // [B][B][B][B][0]
+
+    int Result = StringCount("AAAA");
+
+    //int Count = strlen("BBBB");
+
+    int a = 0;
+}
