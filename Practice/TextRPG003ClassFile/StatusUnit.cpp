93c1d4caf2cb5bad68417b2b1462528188b8db73
성민 류 <baekhun0330@naver.com>
2024-10-07 15:27:53 +0900

    StatusRender 수정

--- Practice/TextRPG003ClassFile/StatusUnit.cpp
+++ Practice/TextRPG003ClassFile/StatusUnit.cpp
@@ -2,13 +2,9 @@
 #include <BaseSystem/EngineDebug.h>
 #include <iostream>
 
-void UStatusUnit::StatusRender()
+void UStatusUnit::TopLine()
 {
-    // 상속관계에서 자식이 존재하는데.
-    // 같은 함수를 사용함에도 자식들마다 다른 동작을 해야할때 사용하는 것이
-    // virtual 
-    
-    // 디버깅상 받는게 유리해서.
+    // TopLine
     const char* Name = GetName();
     //printf_s("%s Status", GetName());
     //int NameLan = static_cast<int>(strlen(GetName()));
@@ -21,13 +17,35 @@ void UStatusUnit::StatusRender()
         printf_s("-");
     }
     printf_s("\n");
-    printf_s("공격력 : %d ~ %d\n", MinAtt, MaxAtt);
-    printf_s("체력 : %d\n", Hp);
+}
 
+void UStatusUnit::BotLine()
+{
+    // BotLine
     for (int i = 0; i < LINECOUNT; i += 1)
     {
         printf_s("-");
     }
     printf_s("\n");
+}
+
+void UStatusUnit::PrintAtt()
+{
+    printf_s("공격력 : %d ~ %d\n", MinAtt, MaxAtt);
+}
+
+void UStatusUnit::PrintHp()
+{
+    printf_s("체력 : %d\n", Hp);
+}
+
+void UStatusUnit::StatusRender()
+{
+    TopLine();
+
+    PrintAtt();
+    PrintHp();
+
+    BotLine();
 }
 
