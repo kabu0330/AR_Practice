4b08ff6f3661b705f5bedecc67b352bd884b5b6d
성민 류 <baekhun0330@naver.com>
2024-10-07 11:43:26 +0900

    TextRPG InterConnecting

--- Practice/TextRPG003ClassFile/Town.cpp
+++ Practice/TextRPG003ClassFile/Town.cpp
@@ -3,7 +3,7 @@
 #include "Player.h"
 
 // 포인터나 레퍼런스는 무조건 8바이트 이기 때문에
-void UTown::InPlayer(class UPlayer& _Player)
+int UTown::InPlayer(class UPlayer& _Player)
 {
 	InMsgPrint();
 
@@ -15,22 +15,27 @@ void UTown::InPlayer(class UPlayer& _Player)
 		std::cout << "0. 체력회복\n";
 		std::cout << "1. 강화\n";
 		std::cout << "2. 사냥터이동\n";
+		std::cout << "3. 중급마을로 이동\n";
 		int Select = _getch();
 
 		switch (Select)
 		{
-		case '0':
-			printf_s("0눌렀습니다.");
-			_getch();
-			break;
-		case '1':
-			printf_s("1눌렀습니다.");
-			_getch();
-			break;
 		case '2':
+		{
+			_Player.SetCurZone(Select - '0');
+			// UFightzone FightZone;
+
 			printf_s("2눌렀습니다.");
 			_getch();
+			return 2;
 			break;
+		}
+		case '0':
+		case '1':
+		case '3':
+			printf_s("아직 완성되지 않은 기능입니다.");
+			_getch();
+			return 1;
 		default:
 			break;
 		}
